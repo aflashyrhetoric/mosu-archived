b4f81156f161d0353b9d7f8d2c5a417c17979caf
Kevin Oh <aflashyrhetoric@gmail.com>
2016-07-01 21:50:45 -0400

    Tweaked to revert something from SIAF

--- public/js/react/app.js
+++ public/js/react/app.js
@@ -1,30 +1,26 @@
-// Start App
-
-(function(App) {
-	var page;
-	var hasToken;
-	// If API_TOKEN is undefined, display login
-	if (! localStorage.getItem("api_token")) {
-		page = <App.Views.Auth />;
-		hasToken = true;
-	} else {
-		// page = <App.Views.Auth />
-		page = 	<App.Views.Mosu 
-					url="/api/v1/jobapplications" 
-					pollInterval="2000" /> ;
-	}
-	ReactDOM.render(
-		(
-			<div>
-				<App.Views.Navbar />
-				{ !hasToken ? 
-					<div className="mosu-container">
-						<App.Views.InspirationBox />
-					</div> : null
-				}
-				{ page }
-			</div>
-		),
-		document.getElementById('app')
-	);
-})(App);
+var page;
+var hasToken;
+// If API_TOKEN is undefined, display login
+if (! localStorage.getItem("api_token")) {
+	page = <App.Views.Auth />;
+	hasToken = true;
+} else {
+	// page = <App.Views.Auth />
+	page = 	<App.Views.Mosu 
+				url="/api/v1/jobapplications" 
+				pollInterval="2000" /> ;
+}
+ReactDOM.render(
+	(
+		<div>
+			<App.Views.Navbar />
+			{ !hasToken ? 
+				<div className="mosu-container">
+					<App.Views.InspirationBox />
+				</div> : null
+			}
+			{ page }
+		</div>
+	),
+	document.getElementById('app')
+);
