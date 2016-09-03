3cbfd5b4331f4936635d21679bfab5635295093b
Kevin Oh <aflashyrhetoric@gmail.com>
2016-09-03 18:19:15 -0400

    Removed scripts.js that was empty

--- public/js/scripts.js
+++
@@ -1,5 +0,0 @@
-// Bootstrap Modal dismissal bug workaround
-// jQuery('[data-dismiss="modal"]').on('click', function(){
-//      jQuery('.modal').hide();
-//      jQuery('.modal-backdrop').hide();
-// });
