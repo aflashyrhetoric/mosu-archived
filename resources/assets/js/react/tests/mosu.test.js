4a314330f1f145f974656d0bc592eccf2dfbee18
Kevin Oh <kevin@nulab-inc.com>
2016-12-11 13:12:17 -0500

    #16
    * Added trivial test to test jest (LOL)

--- resources/assets/js/react/tests/mosu.test.js
+++ resources/assets/js/react/tests/mosu.test.js
@@ -1,5 +1,9 @@
-const sum = require('./sum')
+// const sum = require('./sum')
 
-test('description of test here', () => {
-	expect(sum(1,2)).toBe(3)
+// test('description of test here', () => {
+// 	expect(sum(1,2)).toBe(3)
+// })
+
+test('Silly test to test jest', () => {
+	expect(1+1).toBe(2)
 })
