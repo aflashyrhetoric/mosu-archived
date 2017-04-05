9e1ae3a6889fa66f8ad175a24f834990fab320e2
Kevin Oh <aflashyrhetoric@gmail.com>
2017-04-05 18:28:56 -0400

    #15 test

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -42,6 +42,7 @@
 	<!-- Assorted Scripts -->	
 	{{-- <script type="text/javascript" src="{{ url('js/scripts.js') }}"></script> --}}
 
+
 	<!-- Tracking Code -->
 	<script>
 	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
